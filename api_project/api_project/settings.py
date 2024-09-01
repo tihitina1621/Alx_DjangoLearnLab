3fbf2627bd4e218030ddef2dc1b73fa691642057
tihitina1621 <titawondim@gmail.com>
2024-09-01 04:39:07 +0300

    setting

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'api',
     'rest_framework.authtoken',
-    'rest_framework.authentication.TokenAuthentication'
+    'rest_framework.authentication.TokenAuthentication',
+    'rest_framework.permissions.IsAuthenticated',
 ]
 
 MIDDLEWARE = [
