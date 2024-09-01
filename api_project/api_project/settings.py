97f3686019274804de157083fb0b67b159813b97
tihitina1621 <titawondim@gmail.com>
2024-09-01 04:37:25 +0300

    new comit

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'api',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
+    'rest_framework.authentication.TokenAuthentication'
 ]
 
 MIDDLEWARE = [
