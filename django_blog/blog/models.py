91736c90a27b677a077adb69ef7dca77999d7896
tihitina1621 <titawondim@gmail.com>
2024-09-10 09:07:34 +0300

    nw ap

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,7 +1,5 @@
 from django.db import models
-
-class User(models.Model):
-    post = models.TextField()
+from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
