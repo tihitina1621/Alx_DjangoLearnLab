8d1204e730ef0e0731274036fada5ad54673ef3e
tihitina1621 <titawondim@gmail.com>
2024-09-20 00:25:04 +0300

    d

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -3,5 +3,5 @@ from .models import Book
 
 def book_list(request):      
       books = Book.objects.all()  # Fetch all book instances from the database
-      context = {'list_books': books}  # Create a context dictionary with book list
-      return render(request, 'books/list_books.html', context)
+      context = {'book_list': books}  # Create a context dictionary with book list
+      return render(request, 'relationship_app/list_books.html', context)
