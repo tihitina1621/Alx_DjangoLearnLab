8366421720220c5a292de0f7ea907791a06abd9d
tihitina1621 <titawondim@gmail.com>
2024-09-19 23:29:04 +0300

    v

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -3,8 +3,8 @@ from relationship_app.models import Book, Author, Library
 all_book = Book.objects.all()
 print(all_book)
 
-
-author = Author.objects.get(name='author_name')
+author_name = 'James'
+author = Author.objects.get(name=author_name)
 all = Book.objects.filter(author=author)
 
 library_name = 'new'
