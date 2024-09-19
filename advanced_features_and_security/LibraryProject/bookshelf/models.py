941ff54b1ab9893a40c1d47ab1c4af1490bd640f
tihitina1621 <titawondim@gmail.com>
2024-09-20 02:55:19 +0300

    sdf

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -5,3 +5,7 @@ class Book(models.Model):
     title = models.CharField(max_length=50)
     author = models.CharField(max_length=70)
     publication_year = models.IntegerField()
+class AbstractUser(models.Model):
+    date_of_birth = models.DateField()
+    profile_photo = models.ImageField()
+    
