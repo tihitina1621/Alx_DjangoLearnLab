d0c4f4794317543797f43af2918c915f19edf10b
tihitina1621 <titawondim@gmail.com>
2024-09-19 23:18:18 +0300

    library

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,7 +1,11 @@
-from relationship_app.models import Book, Author
+from relationship_app.models import Book, Author, Library
 
 all_book = Book.objects.all()
 print(all_book)
 
 author = Author.objects.filter(name="James")
 book_author = Book.objects.filter(authors = author)
+
+library = Library.objects.get(name='library_name') 
+all_book = Book.objects.all(name = library)
+print(all_book)
