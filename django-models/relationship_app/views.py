d2af4a821025c75fa3ea7b288df654d7758d31fe
tihitina1621 <titawondim@gmail.com>
2024-09-20 00:34:50 +0300

    dd

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
-from .models import Book
+from django.views.generic import DetailView
+from .models import Book, Library
 
 def book_list(request):      
       books = Book.objects.all()  # Fetch all book instances from the database
       context = {'book_list': books}  # Create a context dictionary with book list
       return render(request, 'relationship_app/list_books.html', context)
+  
+class LibraryDetailView(DetailView):
+  model = Library
+  template_name = 'relationship_app/library_detail.html'
+  context_object_name = 'library'
+def get_context_data(self, **kwargs):    
+    context = super().get_context_data(**kwargs)  # Get default context data
+    book = self.get_object()  # Retrieve the current book instance
+    context['average_rating'] = book.get_average_rating() 
