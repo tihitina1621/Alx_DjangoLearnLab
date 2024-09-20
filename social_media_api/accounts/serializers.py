3a7e8a50cba2a90b13057ce2cde9c6a3740eb6d3
tihitina1621 <titawondim@gmail.com>
2024-09-20 04:07:47 +0300

    shd

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,5 +1,9 @@
 from rest_framework import serializers
 from .models import CustomUser
+from rest_framework.authtoken.models import Token
+
+serializers.CharField()
+Token.objects.create  get_user_model().objects.create_user
 
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
