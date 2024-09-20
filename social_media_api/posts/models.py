d031af3c1a2388dbf26dde3094de2abbcdd5f7cd
tihitina1621 <titawondim@gmail.com>
2024-09-20 16:01:21 +0300

    sdf

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 from .models import User
 
-class Posts(models.Model):
+class Post(models.Model):
     author = models.Foreignkey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     content = models.TextField()
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField(auto_now_add=True)
-class Comments(models.Model):
-    content = models.ForeignKey(Posts, on_delete=models.CASCADE)
+class Comment(models.Model):
+    content = models.ForeignKey(Post, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField(auto_now_add=True)
