445405b027531000c1b22f6d96aa911aa8c98c46
tihitina1621 <titawondim@gmail.com>
2024-09-20 17:31:17 +0300

    my

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -9,6 +9,6 @@ class ULV(generics.GenericAPIView):
 def follow(request):
     following_user = request.user.follows.all()
     posts = Post.objects.filter(author__in=following_users).order_by('created_at')
-    return (following_user, posts)
+    return (following.all(), posts)
 def
 return Response
