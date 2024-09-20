9f5ba5b44f788c1970f709f5653aeb721eed73b0
tihitina1621 <titawondim@gmail.com>
2024-09-20 03:16:51 +0300

    adad

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -6,6 +6,13 @@ class Book(models.Model):
     title = models.CharField(max_length=50)
     author = models.CharField(max_length=70)
     publication_year = models.IntegerField()
+    class Meta:
+        permissions = [
+            ('can_view', 'Can view'),
+            ('can_create', 'Can create'),
+            ('can_edit', 'Can edit'),
+            ('can_delete', 'Can delete'),
+        ]
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField()
     profile_photo = models.ImageField()
