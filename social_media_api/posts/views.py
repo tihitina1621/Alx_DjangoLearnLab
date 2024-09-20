36f3933cc3b422300bde0ad8b36415a0b3345714
tihitina1621 <titawondim@gmail.com>
2024-09-20 16:42:38 +0300

    sdf

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
 
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
 
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
