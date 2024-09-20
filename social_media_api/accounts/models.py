2bcc59f5cbe1ea1b5ec09feb8f74e49eca138c22
tihitina1621 <titawondim@gmail.com>
2024-09-20 04:03:38 +0300

    ne

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -4,6 +4,6 @@ from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 
 class CustomUser(AbstractUser):
-    bio = models.CharField(max_length=200)
+    bio = models.TextField()
     profile_picture = models.ImageField()
     followers = models.ManyToManyField(CustomUser, symmetrical=False)
