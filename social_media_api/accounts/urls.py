0c14de9f3ec58f86222ae88aa73fc86374c0aad2
tihitina1621 <titawondim@gmail.com>
2024-09-20 04:12:09 +0300

    sd

---
+++ social_media_api/accounts/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+from .views import BookList, BookViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'books', BookViewSet)
+
+urlpatterns = [
+    path('register/', admin.site.urls),
+    path('login/', BookList.as_view(), name='new boook'),
+    path('api/', include(router.urls)),
+]
