ca25a8bb0fe4411d0a5a8b78e74befdd8a2b77b8
tihitina1621 <titawondim@gmail.com>
2024-09-20 17:16:25 +0300

    new

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
-# Create your views here.
+from rest_framework import generics, permissions
+from .models import CustomUser
+class ULV(generics.GenericAPIView):
+    queryset = CustomUser.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+    def     
+    return Response
