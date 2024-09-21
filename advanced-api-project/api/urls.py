6a97bb93633cae6482c0416437dbcdb62a5b0f94
tihitina1621 <titawondim@gmail.com>
2024-09-21 14:41:20 +0300

    jjj

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 urlpatters = [
+    path('api.urls, '),
     path('books/<int:pk>', views.book_list, name = 'book_list'),
     path('books/create', views.book_create, name='book_create'),
     path('books/update<int:id>', views.book_update, name='book_update'),
