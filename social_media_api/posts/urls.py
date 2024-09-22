979180376a233fa90c3b25e8eb914b61706a6668
tihitina1621 <titawondim@gmail.com>
2024-09-22 18:41:29 +0300

    sf

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpattrns  = [
-    path('feed/', views.feed_endpoint, name='feed_endpoint',)
+    path('feed/', views.feed_endpoint, name='feed_endpoint',),
+    path("<int:pk>/like/"),
+    path("<int:pk>/unlike/")
 ]
