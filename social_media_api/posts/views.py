ce3484eed6791463e3d110e556ab363ae0eb54d2
tihitina1621 <titawondim@gmail.com>
2024-09-22 18:38:26 +0300

    new

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 #from rest_framework import viewsets, permissions 
-from .models import Post, Comment
+from .models import Post, Comment, Like
 from .serializers import PostSerializer, CommentSerializer
+from django.shortcuts import get_object_or_404
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
@@ -16,3 +17,20 @@ def follow(request):
     posts = Post.objects.filter(author__in=following_users).order_by('created_at')
     permission_classes = [permissions.IsAuthenticated]
     return (following.all(), posts, following_user, permission_classes)
+
+def like(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    like, creat = Like.objects.get_or_create(user=request.user, post=post)
+    if creat:
+        Notification.objects.create(
+            user=post.author,
+            message=f"{request.user.username} liked your post."
+        )
+        message = "Liked"
+    else:
+        like.delete()
+        Notification.objects.create(
+            user=post.author,
+            message=f"{request.user.username} unliked your post."
+        )
+        message = "Unliked"
