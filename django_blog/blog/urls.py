325398c2fbfae4f4fdaea20011c8ccecc20f8ba1
tihitina1621 <titawondim@gmail.com>
2024-09-22 23:47:12 +0300

    df

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('comment/<int:pk>/update/', ),
+    path( "post/<int:pk>/comments/new/",)
+    path("comment/<int:pk>/delete/'),
 ]
